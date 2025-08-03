bc3079164835a77458bed4171dfd7da83106d0b4
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-08-04 00:52:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+// Muhammad Ahmed Ashar
+// 3 / 8 / 2025
+// 17
+
+
+// 1. Create a full name message using variables. Combine first and last name into fullName and alert it.
+// Answer :--
+// var firstName = 'Muhammad Ahmed ';
+// var lastName = 'Ashar';
+// var fullName = firstName + lastName
+// alert(fullName);
+
+// 2. What will be the output of:
+// var a = "5";
+// var b = 2;
+// var result = a + b;
+// console.log(result);
+// Answer :--
+// Because string ko jab number se + krte hain to concatinate hojata ha
+
+// 3. Guess the output and explain:
+//  var a = 10;
+//  var b = 5;
+//  var c = ++a + b-- + --b;
+//  console.log(c);
+// Answer :--
+// var c= ++a + b-- + --b
+// var c = 11 +  5 + 3
+
+// 4. Convert string '20' to a number and multiply it with 3. Show the output
+// Answer :--
+// var stringValue = '20'
+// var numberValue = 3
+// console.log(stringValue * numberValue);
+// 20 ko number mein convert krne ki zarorat nhi kyun ke isko multiply krna ha agar multiply ki jaga plus krna hota to '20' se pehle + lagalete hain phr string number mein convert hojayga
+
+// 5. Write an expression that gives the remainder 1 when divided by 3.
+// Answer :--
+// var number = 4 
+// console.log(number % 3);
+
+// 6. Using the variables below, alert the message:
+//  let student = "Hassan";
+//  let score = 88;
+//  Output: Hassan got 88 marks in JavaScript!
+// Answer :--
+// alert(student + " " + "got " + score + " " + "marks " + "in " + "JavaScript!")
+
+// 7. Combine pre/post increment in one expression and explain the output:
+//  let x = 5;
+//  let y = x++ + ++x - --x;
+//  console.log(y)
+//  Answer :--
+// let y = x++ + ++x - --x;
+//          5  +  7  -  6
+//                6
+
+// 8. Write 5 illegal variable names and correct them
+// Answer :--
+// Illegal variable names:
+// var 1name = "Ahmed";      
+// var full-name = "Ashar";    
+// var var = "Test";         
+// var user name = "Ahmed";   
+// var @email = "abc@xyz";   
+// Corrected variable names:
+// var name1 = "Ahmed";
+// var full_name = "Ashar";
+// var variable = "Test";
+// var user_name = "Ahmed";
+// var email = "abc@xyz";
+
+// 9. A perfume costs 2500 PKR. Write a JS expression to calculate the price of 4 perfumes and alert the bill
+// Answer :--
+// var perfumeCost = 2500;
+// var quantity = 4;
+// var totalBill = perfumeCost * quantity;
+// alert("Total bill for 4 perfumes is: " + totalBill + " PKR");
+
+// 10. Create your own math expression using 3 operators and one variable, then explain it in comments.
+// Bonus: What will be the output of the following code and why?
+//  let msg = "Total: " + (5 + 5) * 2;
+//  alert(msg);
+// Answer :--
+// let msg = "Total: " + (5 + 5) * 2;
+//                          10   * 2
+//                          20
+ 
+
+
