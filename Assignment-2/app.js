c3136f1cab7da2a5a34d065766cdbc76d3252bf5
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-08-10 21:40:28 +0500

    Answer 1

---
+++ Assignment-2/app.js
@@ -0,0 +1,5 @@
+// 1. Write a program that asks the user for their name using prompt and then displays a greeting in an alert box.
+// Answer :--
+// var name = prompt("What Is Your Name?")
+// alert("Hello " + name + " Hope you doing well!!")
+
