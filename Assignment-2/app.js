711de9c6f54458747b6daae8d26f0e2493a154ac
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-08-10 21:40:40 +0500

    Answer 2

--- Assignment-2/app.js
+++ Assignment-2/app.js
@@ -3,3 +3,12 @@
 // var name = prompt("What Is Your Name?")
 // alert("Hello " + name + " Hope you doing well!!")
 
+// 2. Ask the user to enter a number. If the number is greater than 10, display 'Number is big'.
+// Answer :--
+// var number = prompt('Enter any number here')
+// if (number > 10) {
+//     alert(number + " is greater than 10")
+// }else{
+//     alert(number + " is smaller than 10")
+// }
+
