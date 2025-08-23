4e5334d18c8fa8207731d851014c60c7cf08d927
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-08-24 01:51:39 +0500

    Add implementation for removing the first element from an array of city names

--- Assignment-3/app.js
+++ Assignment-3/app.js
@@ -49,6 +49,9 @@
 
 // 7. Write a program that removes the first element of an array of city names and then prints the updated array.
 // Answer :--
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta"];
+// cities.shift();
+// alert(cities);
 
 
 // 8. Create an array of numbers [10, 20, 30, 40, 50]. Use .splice() to remove the middle number (30) and display the array.
