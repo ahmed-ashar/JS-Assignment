1bfd8d2646c7294bddb5afe1f3a61bcac5e39253
Muhammad Ahmed <hafizmahmedashar@gmail.com>
2025-09-29 07:46:37 +0500

    done

--- Assignment-7/app.js
+++ Assignment-7/app.js
@@ -1,18 +1,23 @@
 let is24hour = true;
- 
+
 const clock = document.getElementById('clock');
 const switcher = document.getElementById('switcher');
+
 function updateClock() {
     const time = new Date();
-    let hours = String(time.getHours());
+    let hours = time.getHours();
     const minutes = String(time.getMinutes());
     const seconds = String(time.getSeconds());
     let aMpM = '';
+
     if (!is24hour) {
         aMpM = hours >= 12 ? ' PM' : ' AM';
-        hours = hours % 12 || 12;
+        hours = String(hours % 12 || 12); 
+    } else {
+        hours = String(hours);            
     }
-    clock.textContent = `${hours.padStart(2,'0')}: ${minutes.padStart(2,'0')}:  ${seconds.padStart(2 , "0")}${aMpM}`;
+
+    clock.textContent = `${hours.padStart(2,'0')}:${minutes.padStart(2,'0')}:${seconds.padStart(2,"0")}${aMpM}`;
     setTimeout(updateClock, 1000);
 }
 
@@ -21,4 +26,5 @@ switcher.addEventListener('click', () => {
     switcher.textContent = is24hour ? 'Switch To 12-Hour Format' : 'Switch To 24-Hour Format';
     updateClock();
 });
-    updateClock();
+
+updateClock();
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://js-assignment-three-delta.vercel.app/
 
 Assignment-6:
 https://js-assignment-35aa.vercel.app/
+
+Assignment-7
+https://js-assignment-7ujn.vercel.app/
